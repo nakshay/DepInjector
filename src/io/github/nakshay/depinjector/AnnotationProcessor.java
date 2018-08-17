a8d3d12961ef960ac8148e8af95ccdc0eea22961
Akshay Naik <inbox.akshaynaik@gmail.com>
2018-08-17 12:27:59 +0530

    separate factory method for Instance class

--- src/io/github/nakshay/depinjector/AnnotationProcessor.java
+++ src/io/github/nakshay/depinjector/AnnotationProcessor.java
@@ -15,6 +15,12 @@ import io.github.nakshay.depinjector.annotations.Instance;
 final public class AnnotationProcessor {
 
 	void processAnnotaion(Map<String, Object> map) {
+		prepareClassFactory(map);
+	}
+
+	
+	//create map of class key and class names
+	private void prepareClassFactory(Map<String, Object> map) {
 
 		Reflections reflections = prepareReflection();
 		Set<Class<?>> classNames = reflections.getTypesAnnotatedWith(Instance.class);
@@ -23,10 +29,13 @@ final public class AnnotationProcessor {
 			String name = cls.toString();
 			name = name.substring(name.indexOf(' ') + 1, name.length());
 			map.put(name.substring(name.lastIndexOf('.') + 1, name.length()), name);
-
 		}
-
 	}
+	
+	
+	
+	// returns Reflection instance to scan annotation
+	//This creates copy of loaders twice, rework required
 
 	private Reflections prepareReflection() {
 
